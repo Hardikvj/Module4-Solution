c0867888e85b57203a4312266958d5088986b3b0
Hardikvj <42835366+Hardikvj@users.noreply.github.com>
2020-09-03 15:35:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,7 @@
     for (var i = 0; i < names.length; i++) {
     
       var firstLetter = names[i].charAt(0).toLowerCase();
+        console.log(names[i] + " ");
     
       if (firstLetter === 'j') {
         byeSpeaker.speak(names[i]);
