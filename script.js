cec70e5532938f66df340399c3cd47d8f0eba083
Hardikvj <42835366+Hardikvj@users.noreply.github.com>
2020-09-03 15:38:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
-(function (window) {
-
-    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-    
-    for (var i = 0; i < names.length; i++) {
-    
-      var firstLetter = names[i].charAt(0).toLowerCase();
-        console.log(names[i] + " ");
-    
-      if (firstLetter === 'j') {
-        byeSpeaker.speak(names[i]);
-      } else {
-        helloSpeaker.speak(names[i]);
-      }
-    }
-    
-    })(window);
+console.log("Hello");
